9fc1004e8a2fb3b73988e6a6dada88dbebf81ad7
Michelle-Madiba <130059963+Michelle-Madiba@users.noreply.github.com>
2023-04-06 14:09:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 
 st.title("My first Streamlit app")
 st.write("Streamlit is fun")
+st.write("welcome to my Streamlit app")
+x=st.slider('Select value for x',0,10)
+st.write('You selected X: ',x)
