6dbe6ffaa0c6deffdd3924d4a5f88eff266f77ad
Michelle-Madiba <130059963+Michelle-Madiba@users.noreply.github.com>
2023-04-25 17:17:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import streamlit as st
 import plotly.express as  px
 
-
-
+st.set_page_config(page_title="Flights Dashboard", page_icon=":bar_chart:", layout="wide")
+st.title("My first Streamlit app")
+st.write("Streamlit is fun")
+st.write("welcome to my Streamlit app")
 
